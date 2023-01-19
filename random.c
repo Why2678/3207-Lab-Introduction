cd5547d30fbe19618be5b25bf8abe7746689d2f1
Aaron Jefferson <tug32757@temple.edu>
2023-01-19 18:46:34 -0500

    established a working capital letter generation

--- random.c
+++ random.c
@@ -2,20 +2,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-char randchar();
+char randchar(); // fn prototype
 
-int main(){
-	char my_char = randchar;
+int main()
+{
+	srand((unsigned)time(0));  // seed the random number generator with the current time
+	char my_char = randchar(); // calls the randchar function
 	printf("%c \n", my_char);
-
 }
 
-char randchar(){
-	srand((unsigned)time(0));
-	short asciUpperCase = 65;
-	short numLetters = 26;
+char randchar()
+{
+	short asciUpperCase = 65; // ASCII value for A
+	short numLetters = 26;	  // Letters in the alphabet
 
-	char example = (char)(rand()%numLetters+asciUpperCase);
+	char example = (char)(rand() % numLetters + asciUpperCase);
+	// generates a number between 65 and 90, returns it as a char
 
-	return example;
+	return example; // returns a time based random upper-case letter
 }
